f4158551a33a368fa2c5453791cfd1a18797d3e6
Trung Ho <hmtrung90@gmail.com>
2021-07-22 06:35:46 +0700

    initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,51 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+TutorialPluginAudioProcessorEditor::TutorialPluginAudioProcessorEditor (TutorialPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 200);
+    
+    // these define the parameters of our slider object
+    midiVolume.setSliderStyle (juce::Slider::LinearBarVertical);
+    midiVolume.setRange (0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled (true, false, this);
+    midiVolume.setTextValueSuffix (" Volume");
+    midiVolume.setValue(1.0);
+ 
+    // this function adds the slider to the editor
+    addAndMakeVisible (&midiVolume);
+}
+
+TutorialPluginAudioProcessorEditor::~TutorialPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void TutorialPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
+}
+
+void TutorialPluginAudioProcessorEditor::resized()
+{
+    // sets the position and size of the slider with arguments (x, y, width, height)
+    midiVolume.setBounds (40, 30, 20, getHeight() - 60);
+}
